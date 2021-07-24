6a3c04e2b4d0e84e44c9579281ff6e52a2d55f20
parkyes90 <parkyes90@gmail.com>
2021-07-24 17:29:58 +0900

    refactor: use constant for port

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,11 +3,12 @@ import { createServer } from "http";
 
 const httpServer = createServer();
 const io = new Server(httpServer);
+const PORT = 8000;
 
 io.on("connection", (socket: Socket) => {
   console.log("connected", socket);
 });
 
-httpServer.listen(8000, () => {
-  console.log("Listening PORT:8000");
+httpServer.listen(PORT, () => {
+  console.log(`Listening PORT:${PORT}`);
 });

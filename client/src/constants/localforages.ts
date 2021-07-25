9d8c1a42d9b144690f8388f4b4cca98dab7ee3ae
parkyes90 <parkyes90@gmail.com>
2021-07-25 18:24:22 +0900

    feat: clientId 캐싱

---
+++ client/src/constants/localforages.ts
@@ -0,0 +1,3 @@
+export enum KEY {
+  CLIENT_ID = "clientId",
+}
--- client/src/hooks/useSocket.ts
+++ client/src/hooks/useSocket.ts
@@ -1,24 +1,51 @@
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { FEATURE_COLORS } from "../styles/colors/features";
+import localforage from "localforage";
+import { v4 } from "uuid";
+import { KEY } from "../constants/localforages";
+
+const setSocketInstance = (
+  setSocket: Dispatch<SetStateAction<Socket | null>>,
+  socketInstance: Socket
+) => {
+  socketInstance.on("connect", () => {
+    console.log("%cSocket Connected!", `color: ${FEATURE_COLORS.SUCCESS}`);
+  });
+  socketInstance.on("disconnect", () => {
+    console.log("%cSocket DisConnected!", `color: ${FEATURE_COLORS.SUCCESS}`);
+  });
+  setSocket(socketInstance);
+};
+
+const getSocketInstance = (path: string, clientId: string) => {
+  return io({
+    path,
+    query: {
+      clientId: clientId as string,
+    },
+  });
+};
 
 export const useSocket = (path: string) => {
   const [socket, setSocket] = useState<null | Socket>(null);
   useEffect(() => {
     if (!socket) {
-      const socketInstance = io({
-        path,
+      localforage.getItem(KEY.CLIENT_ID).then((clientId) => {
+        if (clientId) {
+          setSocketInstance(
+            setSocket,
+            getSocketInstance(path, clientId as string)
+          );
+        } else {
+          localforage.setItem(KEY.CLIENT_ID, v4()).then((clientId) => {
+            setSocketInstance(
+              setSocket,
+              getSocketInstance(path, clientId as string)
+            );
+          });
+        }
       });
-      socketInstance.on("connect", () => {
-        console.log("%cSocket Connected!", `color: ${FEATURE_COLORS.SUCCESS}`);
-      });
-      socketInstance.on("disconnect", () => {
-        console.log(
-          "%cSocket DisConnected!",
-          `color: ${FEATURE_COLORS.SUCCESS}`
-        );
-      });
-      setSocket(socketInstance);
     }
   }, [socket, path]);
   return { socket };

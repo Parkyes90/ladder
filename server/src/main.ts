b220b82eda1b3c7ce36f7cfdc4ea7fa09f37db8c
parkyes90 <parkyes90@gmail.com>
2021-07-25 18:24:48 +0900

    feat: Add message transport event

--- server/src/main.ts
+++ server/src/main.ts
@@ -16,8 +16,13 @@ const SDP_OFFER_LIST: Record<string, RTCSessionDescription> = {};
 enum EVENT {
   OFFERS = "offers",
   OFFER_SDP = "offerSDP",
-  OFFER = "offer",
   DELETE_OFFER = "deleteOffer",
+  SEND_MESSAGE = "sendMessage",
+}
+
+enum CLIENT_EVENT {
+  GET_MESSAGE = "getMessage",
+  OFFER = "offer",
 }
 
 io.on("connection", (socket: Socket) => {
@@ -29,8 +34,17 @@ io.on("connection", (socket: Socket) => {
     callback({
       status: 200,
     });
-    socket.broadcast.emit(EVENT.OFFER, { id: socket.id, offer });
+    socket.broadcast.emit(CLIENT_EVENT.OFFER, { id: socket.id, offer });
   });
+  socket.on(EVENT.SEND_MESSAGE, (args, callback) => {
+    const payload = {
+      id: socket.handshake.query.clientId,
+      content: args,
+    };
+    socket.broadcast.emit(CLIENT_EVENT.GET_MESSAGE, payload);
+    callback(payload);
+  });
+
   socket.on("disconnect", () => {
     delete SDP_OFFER_LIST[socket.id];
     socket.broadcast.emit(EVENT.DELETE_OFFER, socket.id);
